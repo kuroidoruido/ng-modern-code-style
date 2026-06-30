e15124cd141bbc59253e0d8142453f5b3e956872
Anthony PENA <anthony.pena@outlook.fr>
2026-06-30 07:10:46 +0200

    fix: ensure base href is correctly handled

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { type ApplicationConfig, provideBrowserGlobalErrorListeners } from "@angular/core";
+import { type ApplicationConfig, EnvironmentProviders, provideAppInitializer, provideBrowserGlobalErrorListeners, Provider } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { APP_BASE_HREF } from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+	providers: [provideBrowserGlobalErrorListeners(), provideAppBaseHref(), provideRouter(routes)],
 };
+
+function provideAppBaseHref(): Provider | EnvironmentProviders {
+	if (location.pathname.length > 1) {
+		return { provide: APP_BASE_HREF, useValue: location.pathname };
+	} else {
+		return { provide: APP_BASE_HREF, useValue: '/' };
+	}
+}
